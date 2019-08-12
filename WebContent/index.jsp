683f0c99792bef04f23a26f3b080d17f2fee20d4
weblyb <limubeen@gamil.com>
2019-08-12 16:31:27 +0900

    0812_02 1) 패키지 생성 2) 서블릿으로 가는 버튼(index.jsp) 및 서블릿(ControlServlet.java (/Controller.do)) 생성

---
+++ WebContent/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<button type="button" onclick="location.href='Controller.do'">게시판</button> <!-- 경로를 /Controller.do로 주면 안된다. -->
+</body>
+</html>
---
+++ src/gdm/controller/ControlServlet.java
@@ -0,0 +1,45 @@
+package gdm.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControlServlet
+ */
+@WebServlet({ "/ControlServlet", "/Controller.do" })
+public class ControlServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControlServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doControl(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doControl(request, response);
+	}
+
+	protected void doControl(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("ControlServlet이 호출 됨!");
+	}
+}
